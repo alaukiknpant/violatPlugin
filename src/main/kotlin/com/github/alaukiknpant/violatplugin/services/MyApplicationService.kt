f941fbf77ebb1f3d8c1f236c897b06e9eebe4ad8
GitHub Action <action@github.com>
2020-12-28 07:37:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/alaukiknpant/violatplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/alaukiknpant/violatplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.alaukiknpant.violatplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alaukiknpant.violatplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/alaukiknpant/violatplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alaukiknpant/violatplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.alaukiknpant.violatplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alaukiknpant.violatplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
